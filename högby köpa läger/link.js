29afffc2f7f00c7f584bdf79b20378c8be803213
SimonStrands <36299106+SimonStrands@users.noreply.github.com>
2018-09-14 16:44:09 +0200

    Add files via upload

---
+++ högby köpa läger/link.js
@@ -0,0 +1,28 @@
+document.getElementById("facebook").addEventListener("click", gotofacebook);
+
+function gotofacebook()
+{
+    window.location.href = "https://www.facebook.com/hogbyiffriidrott";
+}
+/////////////////////////////////////////////////
+document.getElementById("Hogby").addEventListener("click", gotohogby);
+
+function gotohogby()
+{
+    window.location.href = "http://www.hogbyif.se/friidrott/";
+}
+//////////////////////////////////////////////////
+document.getElementById("olandrun").addEventListener("click", gotoolandrun);
+
+function gotoolandrun()
+{
+    window.location.href = "http://www.olandrunning.se/";
+}
+//////////////////////////////////////////////////
+document.getElementById("irunkalmar").addEventListener("click", gotoirunkalmar);
+
+function gotoirunkalmar()
+{
+    window.location.href = "http://irunkalmar.com/";
+}
+
